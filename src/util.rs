f0bba4e9747d2925e2e5cce28ddda196ff116b12
zxkmm <zxkmm@hotmail.com>
2024-04-04 12:24:57 +0800

    textual

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,6 @@
 use std::str;
 
-use std::process::{Command, Stdio};
-use std::io::{BufRead, BufReader};
+use std::process::{Command};
 
 pub fn run_command(cmd: &str, args: Vec<&str>) -> Result<String, std::io::Error> {
     let mut command = Command::new(cmd);
