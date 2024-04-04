b722e5dbc6a5c6541dd9085cda8366d395c126eb
zxkmm <zxkmm@hotmail.com>
2024-04-04 11:02:39 +0800

    output push text

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ fn main() -> io::Result<()> {
     let check_result = util::check_username_in_repo(&username, &destination_repo);
     match check_result {
         true => {
-            println!("---\nDestination repo contains username");
-            util::run_command("git", vec!["push"])?;
+            println!("---\nLooks safe to push, pushing...");
+            let push_output = util::run_command("git", vec!["push"])?;
+            println!("---\n{}", push_output);
+
         }
         false => {
             println!("---\nYou are pushing into not your own repo, ABORT.");
